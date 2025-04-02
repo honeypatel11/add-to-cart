632d6823d756e701fe458148adc666ae8b64f300
honeypatel11 <honeypatel1109@gmail.com>
2025-04-02 16:59:41 +0530

    added new section

--- assets/js/index.js
+++ assets/js/index.js
@@ -9,7 +9,7 @@ let products = [
     {
         id: 2,
         name: "Artificial Jewellery",
-        image: "https://d-themes.com/html/riode/images/demos/demo-diamart/products/6-1.jpg",
+        image: "https://d-themes.com/html/riode/images/demos/demo-diamart/products/2-1.jpg",
         price: 170.00
 
     },
@@ -27,27 +27,22 @@ let products = [
         price: 215.00
 
     },
-    {
-        id: 5,
-        name: "Women's Platinum Ring",
-        image: "https://d-themes.com/html/riode/images/demos/demo-diamart/products/5-1.jpg",
-        price: 230.00
 
-    },
     {
         id: 6,
         name: "Women's Platinum Ring",
-        image: "https://d-themes.com/html/riode/images/demos/demo-diamart/products/5-1.jpg",
+        image: "https://d-themes.com/html/riode/images/demos/demo-diamart/products/4-1.jpg",
         price: 230.00
 
     },
     {
         id: 7,
         name: "Women's Platinum Ring",
-        image: "https://d-themes.com/html/riode/images/demos/demo-diamart/products/5-1.jpg",
+        image: "https://d-themes.com/html/riode/images/demos/demo-diamart/products/1-1.jpg",
         price: 230.00
 
     },
+
 
 ]
 function addToCart(productid) {
@@ -57,18 +52,14 @@ function addToCart(productid) {
 let productElement = document.getElementById("product");
 products.forEach((product, idx) => {
     productElement.innerHTML += `
-     <div class="col-3">       
-     <div>
-     <img src="${product.image}" alt="${product.name}">
-     </div>
-     <div class="text-body-secondary my-2 fw-bold">
-     <h6>${product.name}</h6>
-     </div>
-     <div class="fw-bold d-flex justify-content-between align-content-center fw-bold">
-     <span>$ ${product.price}</span>
-     <button class="btn btn-danger" onclick="addToCart(${product.id})">Add To Cart</button>
-     </div>
-     </div>
+     <div class="col-md-4 mb-4">
+                        <div class="product-card text-center shadow-sm">
+                            <img src="${product.image}" alt="${product.name}" class="img-fluid product-image mb-3">
+                            <h5 class="text-dark fw-bold">${product.name}</h5>
+                            <p class="text-danger fw-bold">$${product.price}</p>
+                            <button class="btn btn-success" onclick="addToCart(${product.id})">Add To Cart</button>
+                        </div>
+                    </div>
             `
 })
 
